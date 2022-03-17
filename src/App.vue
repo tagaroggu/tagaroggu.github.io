d63d859bab918039804ac07f99733bc06954aaec
Logan <58315587+tagaroggu@users.noreply.github.com>
2022-03-17 09:06:40 -0500

    Setup vite by hand because I don't have a dedicated environment

---
+++ src/App.vue
@@ -0,0 +1,6 @@
+<script setup>
+import Navbar from './components/Navbar.vue';
+</script>
+<template>
+    <Navbar />
+</template>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,22 @@
+<script setup>
+    const showing = ref(false);
+</script>
+<template>
+    <nav class="navbar">
+        <div class="navbar-brand">
+            <a role="button" class="navbar-burger" :class="{ 'is-active': showing }" @click="showing = !showing">
+                <span aria-hidden="true"></span>
+                <span aria-hidden="true"></span>
+                <span aria-hidden="true"></span>
+            </a>
+        </div>
+        <div class="navbar-menu" :class="{ 'is-active': showing }">
+            <div class="navbar-start">
+
+            </div>
+            <div class="navbar-end">
+
+            </div>
+        <div>
+    </nav>
+</template>
